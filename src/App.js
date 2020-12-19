cab3b2b874c8daa29d03488ccf3e53e3f36a3fb2
Sadiq <samin.sada@gmail.com>
2020-12-19 02:04:59 -0500

    auth server side done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import GenerateCard from './components/GenerateCard';
+import UserContext from './context/UserContext';
+import Axios from 'axios';
 
 function App() {
-  return <GenerateCard />;
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem('auth-token');
+      if (token === null) {
+        localStorage.setItem('auth-token', '');
+        token = '';
+      }
+      const tokenRes = await Axios.post(
+        'http://localhost:5000/users/tokenIsValid',
+        null,
+        {
+          headers: { 'x-auth-token': token },
+        }
+      );
+
+      if (tokenRes.data) {
+        const userRes = await Axios.get('http://localhost:5000/users/', {
+          headers: { 'x-auth-token': token },
+        });
+      }
+
+      setUserData({
+        token,
+        user: userRes.data,
+      });
+    };
+
+    checkLoggedIn();
+  }, []);
+
+  return (
+    <>
+      <BrowserRouter>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path='/register' component={Register} />
+            <Route path='/generate' component={GenerateCard} />
+          </Switch>
+        </UserContext.Provider>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
